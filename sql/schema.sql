39fbc3eb76514039df26cbc3596388c54c8c2ac0
Lyudmila Lebedeva <petrenko1171@gmail.com>
2020-02-16 20:03:23 +0300

    updated sql schema

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,7 +1,9 @@
-CREATE TABLE IF NOT EXISTS wishes (
+CREATE DATABASE db CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE db;
+CREATE TABLE wishes (
                                        id INT AUTO_INCREMENT PRIMARY KEY,
                                        wish TEXT,
                                        link TEXT,
                                        description TEXT,
                                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)  ENGINE=INNODB;
+);
