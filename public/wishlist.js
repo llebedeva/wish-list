e0a104641ff04d381dcebf626703aa350da803fb
Lyudmila Lebedeva <petrenko1171@gmail.com>
2020-03-23 19:13:17 +0300

    refacktoring js code

--- public/wishlist.js
+++ public/wishlist.js
@@ -1,37 +1,27 @@
-// Get the modal
-const modal = document.getElementById("modal");
+const createBtn = document.querySelector('#createButton');
+const editBtns = document.querySelectorAll('button[name="edit"]');
 
-// Get the button that opens the modal
-const createBtn = document.getElementById("createButton");
+const modal = document.querySelector('#modal');
+const span = document.querySelector('.close');
+const addBtn = modal.querySelector('#add');
+const updateBtn = modal.querySelector('#update');
 
-// Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 createBtn.onclick = function() {
     addBtn.style.display = 'block';
     updateBtn.style.display = 'none';
 
-    modal.style.display = "block";
+    modal.style.display = 'block';
 };
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modal.style.display = 'none';
 };
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target === modal) {
-        modal.style.display = "none";
+        modal.style.display = 'none';
     }
 };
-
-const editBtns = document.querySelectorAll('button[name="edit"]');
-
-const addBtn = modal.querySelector('#add');
-
-const updateBtn = modal.querySelector('#update');
 
 editBtns.forEach((button) => {
     button.onclick = (event) => {
