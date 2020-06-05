927b65aab66d1ba156f44eb37f1f8951b9a2cd38
Lyudmila Lebedeva <petrenko1171@gmail.com>
2020-06-05 18:20:31 +0300

    ready to change nginx config

---
+++ public/js/main.js
@@ -0,0 +1,21 @@
+const NotFound = { template: '<p>Страница не найдена</p>' }
+const Home = { template: '<p>home</p>' }
+const Wish = { template: '<p>wish</p>' }
+
+const routes = {
+    '/': Home,
+    '/wish': Wish
+}
+
+new Vue({
+    el: '#app',
+    data: {
+        currentRoute: window.location.pathname
+    },
+    computed: {
+        ViewComponent () {
+            return routes[this.currentRoute] || NotFound
+        }
+    },
+    render (h) { return h(this.ViewComponent) }
+})
